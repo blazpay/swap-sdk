0a231a6b4267c3b25c91b8c1bd693fda0bf0c805
Ashu <ashutoshm9695@gmail.com>
2024-11-18 13:37:34 +0530

    1.1.6

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -98,22 +98,22 @@ export const relayerAddresses: { [key: number]: string } = {
   56: "0x0000000000000000000000000000000000000000",
   137: "0x9B3471de09Df61983AffbC044A99F56dEc51dE3E",
   1: "0x0000000000000000000000000000000000000000",
-  42161: "0x0000000000000000000000000000000000000000"
-}
+  42161: "0x0000000000000000000000000000000000000000",
+};
 
 export const MESSAGE_TYPES = {
   EIP712Domain: [
-      { name: "name", type: "string" },
-      { name: "version", type: "string" },
-      { name: "chainId", type: "uint256" },
-      { name: "verifyingContract", type: "address" }
+    { name: "name", type: "string" },
+    { name: "version", type: "string" },
+    { name: "chainId", type: "uint256" },
+    { name: "verifyingContract", type: "address" },
   ],
   MetaTransaction: [
-      { name: "user", type: "address" },
-      { name: "targetContract", type: "address" },
-      { name: "data", type: "bytes" },
-      { name: "nonce", type: "uint256" },
+    { name: "user", type: "address" },
+    { name: "targetContract", type: "address" },
+    { name: "data", type: "bytes" },
+    { name: "nonce", type: "uint256" },
   ],
 };
 
-export const baseUrl = "http://localhost:5000/api/defi" 
+export const baseUrl = "https://api-v2.blazpay.com/api/defi";
