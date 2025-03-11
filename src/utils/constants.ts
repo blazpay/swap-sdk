82d69d3ea1d39a8b1dbc83ff63fbf79626cff67e
bishthub <1803013103@ipec.org.in>
2025-03-11 06:48:45 +0530

    v1.3.9

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -103,7 +103,7 @@ export const addressE = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 // }
 export const relayerAddresses = (chain: number):string => {
   if(chain == 137) 
-    return '0x753099447d68Ce61044C0f66a2941E87b5Cf4421'
+    return '0xa851D4125cC029743F371eaD4F9DdE4BA04F5146'
   else return '0x10c112A6032dC97ec8854457FA3961D29dB2045A'
 }
 
